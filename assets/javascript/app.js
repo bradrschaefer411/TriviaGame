db1b5b6d47e1df4e8671d4a5f5bbe6b1f64f76be
Brad Schaefer <bradrschaefer411@gmail.com>
2020-03-14 17:19:59 -0400

    Not submitting, just pushing it up so I can edit it elsewhere

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+var start = $("start");
+var done = $("done");
+var questionSlide = $("#questionSlide");
+var startSlide = $("#startSlide");
+var winSlide = $("#winSlide");
+var loseSlide = $("#loseSlide");
