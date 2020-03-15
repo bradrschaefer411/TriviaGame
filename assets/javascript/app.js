acf5fa6dfcc94f8c46f40ae868970fd81053d503
Brad Schaefer <bradrschaefer411@gmail.com>
2020-03-15 14:55:46 -0400

    uploadin' to edit elsewhere

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,18 @@
 var start = $("start");
-var done = $("done");
-var questionSlide = $("#questionSlide");
 var startSlide = $("#startSlide");
-var winSlide = $("#winSlide");
-var loseSlide = $("#loseSlide");
+var timeRemain = document.getElementById("txt");
+
+setTimeout(function(){ timeRemain.value = "2 seconds" }, 2000);
+setTimeout(function(){ timeRemain.value = "4 seconds" }, 4000);
+setTimeout(function(){ timeRemain.value = "6 seconds" }, 6000);
+
+
+start.on("click", function () {
+    startSlide.hide();
+    questionSlide.show();
+});
+
+// var questionSlide = $("#questionSlide");
+// var startSlide = $("#startSlide");
+// var winSlide = $("#winSlide");
+// var loseSlide = $("#loseSlide");
