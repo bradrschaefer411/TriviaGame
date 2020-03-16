dd6614da14d9460d01a55e765de2485d8ba76daf
Brad Schaefer <bradrschaefer411@gmail.com>
2020-03-16 07:35:49 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,23 @@
-
 $(document).ready(function(){
   
-    // event listeners
     $("#timeRemain").hide();
-    $("#start").on('click', startGame);
+    $("#start").on('click', trivia.startGame);
     $(document).on('click' , '.option', trivia.guessChecker);
     
   })
-  var start = $("start");
-var menu = $("#menu");
+
+  trivia.currentSet = 0;
+  trivia.correct = 0;
+  trivia.incorrect = 0;
+  trivia.unanswered = 0;
+  clearInterval(trivia.timeRemain);
+
+  $('#game').show();
+    
+    $('#results').html('');
+    
+    $('#timeRemain').text(trivia.timeRemain);
+
 var timeRemain = 30;
 function startTimer() {
     start.hide();
@@ -22,47 +31,57 @@ function startTimer() {
         timerOn: false,
         timerId : '',
 
+
+        
         questions: {
-          q1: 'A Dewgong with Thick Fat is most resistant to what type?',
-          q2: 'Which Pokemon is capable of doing the most damage in the entire game under the right circumstances?',
-          q3: 'A Choice Band Swampert with max attack investment and an Adamant nature is fighting a Ninetales with Drought. This sun is active as a result. The Ninetales has a Timid nature, 120 points invested into defense and 252 points invested into HP. How much damage does Swampert do to Ninetales with a Waterfall?',
-          q4: 'How much damage does 2 layers of Spikes do to an opponent?',
-          q5: 'Which of these type combinations has the most amount of double resistances?',
-          q6: 'Which of these moves has the highest priority?'
-          q7: 'In Generation I, if a Sandslash uses Sword Dance 3 times and then uses Slash on a Snorlax, how much damage will it do?'
+          q1: "In Generation I, if a Sandslash uses Sword Dance 3 times and then uses Slash on a Snorlax, how much damage will it do?",
+          q2: "Which Pokemon is capable of doing the most damage in the entire game under the right circumstances?",
+          q3: "A Choice Band Swampert with max attack investment and an Adamant nature is fighting a Ninetales with Drought. This sun is active as a result. The Ninetales has a Timid nature, 120 points invested into defense and 252 points invested into HP. How much damage does Swampert do to Ninetales with a Waterfall?",
+          q4: "How much damage does 2 layers of Spikes do to an opponent?",
+          q5: "Which of these type combinations has the most amount of double resistances?",
+          q6: "Which of these moves has the highest priority?"
         },
         options: {
-          q1: ['Ice', 'Fire', 'Water', 'Steel'],
+          q1: ['24.4 - 28.8%', '83.5 - 98.2%', '80.4 - 94.6%', '26.9 - 31.7%'],
           q2: ['Stakataka', 'Mega Mawile', 'Mega Mewtwo X', 'Shuckle'],
           q3: ['59.7 - 70.5%', '58.8 - 70.2%', '89.1 - 105.1%', '120 - 141.1%'],
           q4: ['12.5%', '21.2%', '25%', '16.67%'],
           q5: ['Water/Dragon','Electric/Steel','Steel/Flying','Fire/Steel'],
           q6: ['Vacuum Wave', 'Vital Throw', 'Ally Switch', 'Baby-Doll Eyes']
-          q7: ['24.4 - 28.8%', '83.5 - 98.2%', '80.4 - 94.6%', '26.9 - 31.7%']
         },
         answers: {
-          q1: 'Ice',
+          q1: '24.4 - 28.8%',
           q2: 'Shuckle',
           q3: '58.8 - 70.2%',
           q4: '16.67%',
           q5: 'Fire/Steel',
           q6: 'Ally Switch',
-          q7: '24.4 - 28.8%'
         }
 }
-//calls start Game function
-startGame();
 
-//function for starting game: show time remaining
-//random points for gems to be added to scoreDisplay
-function startGame() {
-    timeRemain = 30;
-    CountdownInSeconds();
 
+$('#start').hide();
+
+$('#TimeRemain').show();
+
+trivia.nextQuestion();
+
+trivia.timeRemain = 40;
+$('#timeRemain').removeClass('last-seconds');
+$('#timeRemain').text(trivia.timer);
+
+if(!trivia.timerOn){
+ trivia.timerId = setInterval(trivia.timerRunning, 4000);
 }
-//function to countdown in seconds 
-function countdownInSeconds() {
-    document.getElementById("#secondsCountdown").innerHTML = "Time Remaining: " + timeRemain; alert("Yo");
-//$("#start").click(function(){
-  //     "Time remaining: " + timeRemain; 
-//});
+
+var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+$('#question').text(questionContent);
+
+var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    
+$.each(questionOptions, function(index, key){
+  $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+})
+
+
+};
