92d3b4edb030b95f7c4894e3a2a29e1818a9d2fb
Brad Schaefer <bradrschaefer411@gmail.com>
2020-03-15 20:10:27 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,60 @@
 var start = $("start");
-var startSlide = $("#startSlide");
-var timeRemain = document.getElementById("txt");
+var menu = $("#menu");
+var timeRemain = 30;
+function startTimer() {
+    start.hide();
+    showTimer.show();
+    var trivia = {
+        correct: 0,
+        incorrect: 0,
+        unanswered: 0,
+        currentSet: 0,
+        timeRemain: 0,
+        timerOn: false,
+        timerId : '',
 
-setTimeout(function(){ timeRemain.value = "2 seconds" }, 2000);
-setTimeout(function(){ timeRemain.value = "4 seconds" }, 4000);
-setTimeout(function(){ timeRemain.value = "6 seconds" }, 6000);
+        questions: {
+          q1: 'A Dewgong with Thick Fat is most resistant to what type?',
+          q2: 'Which Pokemon is capable of doing the most damage in the entire game under the right circumstances?',
+          q3: 'A Choice Band Swampert with max attack investment and an Adamant nature is fighting a Ninetales with Drought. This sun is active as a result. The Ninetales has a Timid nature, 120 points invested into defense and 252 points invested into HP. How much damage does Swampert do to Ninetales with a Waterfall?',
+          q4: 'How much damage does 2 layers of Spikes do to an opponent?',
+          q5: 'Which of these type combinations has the most amount of double resistances?',
+          q6: 'Which of these moves has the highest priority?'
+        },
+        options: {
+          q1: ['Ice', 'Fire', 'Water', 'Steel'],
+          q2: ['Stakataka', 'Mega Mawile', 'Mega Mewtwo X', 'Shuckle'],
+          q3: ['59.7 - 70.5%', '58.8 - 70.2%', '89.1 - 105.1%', '120 - 141.1%'],
+          q4: ['12.5%', '21.2%', '25%', '16.67%'],
+          q5: ['Water/Dragon','Electric/Steel','Steel/Flying','Fire/Steel'],
+          q6: ['Vacuum Wave', 'Vital Throw', 'Ally Switch', 'Baby-Doll Eyes']
+        },
+        answers: {
+          q1: 'Ice',
+          q2: 'Shuckle',
+          q3: '58.8 - 70.2%',
+          q4: '16.67%',
+          q5: 'Fire/Steel',
+          q6: 'Ally Switch'
+        }
+}
+//calls start Game function
+startGame();
 
-
-start.on("click", function () {
-    startSlide.hide();
-    questionSlide.show();
-});
-
-// var questionSlide = $("#questionSlide");
-// var startSlide = $("#startSlide");
-// var winSlide = $("#winSlide");
-// var loseSlide = $("#loseSlide");
+//function for starting game: show time remaining
+//random points for gems to be added to scoreDisplay
+function startGame() {
+    timeRemain = 30;
+    CountdownInSeconds();
+    updateTotalScore();
+    updateWinsTotal();
+    updateLossesTotal();
+    mineralPoints(1, 12);
+    totalScore = 0;
+}
+//function to countdown in seconds 
+function countdownInSeconds() {
+    document.getElementById("#secondsCountdown").innerHTML = "Time Remaining: " + timeRemain; alert("Yo");
+//$("#start").click(function(){
+  //     "Time remaining: " + timeRemain; 
+//});
